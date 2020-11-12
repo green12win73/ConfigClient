3ce9b53081e809a4108a37cf2d7837e1a1f859b6
fangqi <fangqi@taihe.com>
2020-11-12 20:27:37 +0800

    配置中心

--- src/main/java/com/config/client/controller/ConfigClientController.java
+++ src/main/java/com/config/client/controller/ConfigClientController.java
@@ -1,11 +1,16 @@
 package com.config.client.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+//可以手动刷新配置
+@RefreshScope
 //${}中的配置都来源于配置中心
-@Controller
+@RestController
 @RequestMapping(value = "/client")
 public class ConfigClientController {
     @Value("${welcome}")
@@ -14,7 +19,6 @@ public class ConfigClientController {
     private String env;
     @Value("${user}")
     private String user;
-
     @RequestMapping(value = "/info")
     @ResponseBody
     public String info(){
